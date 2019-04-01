7214981d0609d8ba3993bf0cd357239372224563
SifuUA <alekseykres@gmail.com>
2019-04-01 11:24:07 +0300

    implemented exceptions

--- basis/src/main/java/com/okres/basis/exception/NotEqualCheckSumExeption.java
+++ basis/src/main/java/com/okres/basis/exception/NotEqualCheckSumExeption.java
@@ -1,4 +1,7 @@
 package com.okres.basis.exception;
 
 public class NotEqualCheckSumExeption extends Exception {
+    public NotEqualCheckSumExeption(String message) {
+        super("The checksum is not equal!");
+    }
 }
--- basis/src/main/java/com/okres/basis/exception/NotFindInRoutsException.java
+++ basis/src/main/java/com/okres/basis/exception/NotFindInRoutsException.java
@@ -1,4 +1,7 @@
 package com.okres.basis.exception;
 
 public class NotFindInRoutsException extends Exception {
+    public NotFindInRoutsException(String message) {
+        super("There is no such id in Hash map!");
+    }
 }
